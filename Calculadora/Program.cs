ea0ecfd34fd6b711de86bcd4ac2deba0fe67dd99
Ruben Arias <RUBEN@INICIO3D>
2023-03-13 19:53:25 -0300

    Agregar archivos de proyecto.

---
+++ Calculadora/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2, result;
+            char operation;
+            string anotherOp;
+
+            Console.WriteLine("Welcome to the Calculator!");
+
+            do
+            {
+                Console.Write("Enter the first number: ");
+                num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter the second number: ");
+                num2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter the operation (+, -, *, /): ");
+                operation = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                switch (operation)
+                {
+                    case '+':
+                        result = num1 + num2;
+                        Console.WriteLine($"The sum of {num1} and {num2} is {result}.");
+                        break;
+
+                    case '-':
+                        result = num1 - num2;
+                        Console.WriteLine($"The difference between {num1} and {num2} is {result}.");
+                        break;
+
+                    case '*':
+                        result = num1 * num2;
+                        Console.WriteLine($"The product of {num1} and {num2} is {result}.");
+                        break;
+
+                    case '/':
+                        if (num2 == 0)
+                        {
+                            Console.WriteLine("Error: division by zero.");
+                        }
+                        else
+                        {
+                            result = num1 / num2;
+                            Console.WriteLine($"The quotient of {num1} and {num2} is {result}.");
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Error: invalid operation.");
+                        break;
+                }
+
+                Console.Write("Do you want to perform another operation? (Yes/No): ");
+                anotherOp = Console.ReadLine().ToLower();
+
+                if (anotherOp == "yes")
+                {
+                    Console.Clear();
+                }
+
+            } while (anotherOp == "yes");
+
+            Console.WriteLine("Thank you for using the Calculator!");
+            Console.ReadKey();
+        }
+    }
+}
+
